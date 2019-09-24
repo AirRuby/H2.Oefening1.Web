e2467b3a37c1c544640843581c20925e60a28d88
niels vanden broecke <niels.vanden.broecke@hotmail.com>
2019-09-24 16:09:18 +0200

    Add project files.

---
+++ H2.Oefening1.Web/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace H2.Oefening1.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Whatsmyname()
+        {
+            return Content($"Niels Vanden Broecke {DateTime.Now}");
+        }
+
+        public IActionResult Index()
+        {
+
+            string links = "<ul>" +
+                                "<li><a href='http://www.google.be'> Google </a></li> " +
+                                "<li><a href='http://www.hln.be'> HLN </a></li> " +
+                                "<li><a href='http://www.facebook.be'> facebook </a></li> " +
+                                "<li><a href='https://localhost:44319/Home/Whatsmyname'> Whatsmyname </a></li> " +
+                                "<li><a href='https://localhost:44319/ReponseTypes/File'> File </a></li> " +
+                                "<li><a href='https://localhost:44319/ReponseTypes/Forward'> Forward </a></li> " +
+                            "</ul>";
+            return Content(links, "text/html");
+        }
+    }
+}
---
+++ H2.Oefening1.Web/Controllers/ReponseTypesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace H2.Oefening1.Web.Controllers
+{
+    public class ReponseTypesController : Controller
+    {
+        public IActionResult Fail()
+        {
+            return NotFound();
+        }
+
+        public IActionResult File()
+        {
+            string path = "~/PPT/Oefeningen-Controllers.pptx";
+            string content = "application/vnd.ms-powerpoint";
+            return File(path, content);
+        }
+
+        public IActionResult Forward()
+        {
+            return Redirect("File");
+        }
+    }
+}
